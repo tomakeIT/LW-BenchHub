6d059c90df2d020be9c6bb41f627272d3798c128
yida <niuyida@bigai.ai>
2025-08-06 14:54:09 +0800

    [bugfix] fix missing third_party folder

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ pip install torch==2.5.1 torchvision==0.20.1 --index-url https://download.pytorc
 pip install --upgrade pip
 pip install 'isaacsim[all,extscache]==4.5.0' --extra-index-url https://pypi.nvidia.com
 
+# create third_party dir
+mkdir -p third_party
+
 # IsaacLab
 cd third_party
 git clone https://github.com/isaac-sim/IsaacLab.git
